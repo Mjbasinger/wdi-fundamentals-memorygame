635cf17021b42e9a49496404e4f14b86ac2e1b34
Michael Basinger <michaelbasinger@Michaels-MacBook-Pro.local>
2019-08-23 08:54:28 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,23 @@
-console.log("Up and running!");
 
-//console.log("user flipped " + cardOne);
-//console.log("user flipped " + cardThree);
 
 var cards = ["queen", "queen", "king", "king"];
-let cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[3];
-	cardsInPlay.push(cardOne);
-		console.log("User flipped queen");
-	cardsInPlay.push(cardTwo);
-		console.log("user flipped king");
-	if (cardsInPlay.length === 2) {	
-		console.log("Two cards in play.");
-} 
+
+var cardsInPlay =[];
+
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("you've found a match!"); 
-} else if (cardsInPlay[0] !== cardsInPlay[1]) {
-	alert("Sorry, try again!");
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again.");
 }
+};
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+	}
 
-
+flipCard(0);
+flipCard(2);
